71eb84f5b3d4d554607d8ad7d37c8b625353f758
Nick Handy <nichandy@gmail.com>
2020-12-29 18:12:20 -0800

    Update npm packages and add support for webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/env',
+            '@babel/react',
+            {
+              plugins: ['@babel/plugin-proposal-class-properties'],
+            },
+          ],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 5000,
+    publicPath: 'http://localhost:5000/dist/',
+    hotOnly: true,
+    overlay: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin(), new Dotenv()],
+};
