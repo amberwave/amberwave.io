51cd2b6792a66bae3fa0bab74d8ae585d0e92a6d
Nick Handy <nichandy@gmail.com>
2021-01-26 19:08:13 -0800

    create node reducder

---
+++ client/src/reducers/nodesReducer.js
@@ -0,0 +1,11 @@
+//import action
+const nodesReducerDefaultState = [];
+
+const nodesReducer = (state = nodesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default nodesReducer;
