965f7fc1df0a061b5c4fa2eb27c2941fefc452ac
Nick Handy <nichandy@gmail.com>
2021-02-14 15:15:51 -0800

    define node actions to retrieve node(s)

--- client/src/actions/nodeActions.js
+++ client/src/actions/nodeActions.js
@@ -1,18 +1,66 @@
-// ADD_EXPENSE Action Generator
-export const addExpense = (expense) => ({
-  type: 'ADD_EXPENSE',
-  expense,
+import axios from 'axios';
+
+import {
+  GET_NODE,
+  GET_NODES,
+  CREATE_NODE,
+  REMOVE_NODE,
+  EDIT_NODE,
+} from './types';
+
+// GET_NODE Action Generator
+export const getNode = () => (dispatch) => {
+  dispatch(setNodeLoading());
+  axios
+    .get('/api/node')
+    .then((res) =>
+      dispatch({
+        type: GET_NODE,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_NODE,
+        payload: {},
+      })
+    );
+};
+
+// GET_NODES
+export const getNodes = () => (dispatch) => {
+  dispatch(setNodeLoading());
+  axios
+    .get('/api/nodes')
+    .then((res) =>
+      dispatch({
+        type: GET_NODES,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_NODES,
+        payload: {},
+      })
+    );
+};
+
+// CREATE_NODE Action Generator
+export const createNode = (node) => ({
+  type: CREATE_NODE,
+  node,
 });
 
-// REMOVE_EXPENSE Action Generator
+// REMOVE_NODE Action Generator
 export const removeNode = ({ id } = {}) => ({
-  type: 'REMOVE_EXPENSE',
+  type: REMOVE_NODE,
   id,
 });
 
-// EDIT_EXPENSE Generator
+// EDIT_NODE Generator
 export const editNode = (id, updates) => ({
-  type: 'EDIT_EXPENSE',
+  type: EDIT_NODE,
   id,
   updates,
 });
